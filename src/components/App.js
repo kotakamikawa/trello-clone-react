32d69b2d50c74f494aea6368c0a264b1c239c892
kotakamikawa <kamikawat8@gmail.com>
2020-02-07 18:31:03 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+      <TrelloList title={"test"} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrelloCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, Typography } from "@material-ui/core";
+
+const TrelloCard = () => {
+  return (
+    <Card>
+      <Typography gutterBottom>Word of the Day</Typography>
+    </Card>
+  );
+};
+
+export default TrelloCard;
