05b8dc57da56cba5ddd110dedf9bbdd35a304b96
kotakamikawa <kamikawat8@gmail.com>
2020-03-16 22:16:17 +0900

    Updates

---
+++ precache-manifest.6417affc1db9534dfb9b263a22c04112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a00e1a8429e88e198b54d7841c55eb",
+    "url": "/trello-clone-react/index.html"
+  },
+  {
+    "revision": "1b3be30bf37c313eae48",
+    "url": "/trello-clone-react/static/js/2.ec79a8a5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/trello-clone-react/static/js/2.ec79a8a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d88b95baa68bcab5de",
+    "url": "/trello-clone-react/static/js/main.71339613.chunk.js"
+  },
+  {
+    "revision": "5b31ee5e207df3d40a36",
+    "url": "/trello-clone-react/static/js/runtime-main.4a99eca8.js"
+  }
+]);
